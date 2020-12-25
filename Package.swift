aeff06cd4b776c6cb5c29e6feacf23e8a8658776
Dmitrii <zeionara@gmail.com>
2020-12-25 16:01:43 +0300

    Updated PerfectLib version

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/PerfectSideRepos/Perfect-CMongo.git", from: "0.1.0"),
 		.package(url: "https://github.com/PerfectSideRepos/Perfect-CBSON.git", from: "0.0.0"),
-		.package(url: "https://github.com/PerfectlySoft/PerfectLib.git", from: "3.0.0")
+		.package(url: "https://github.com/PerfectlySoft/PerfectLib.git", from: "4.0.0")
 	],
 	targets: [
 		.target(name: "PerfectMongoDB", dependencies: ["PerfectLib"]),
